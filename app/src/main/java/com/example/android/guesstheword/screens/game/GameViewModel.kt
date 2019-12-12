a41a559c4f577a8bdf1a0e09a5b4bf9852f7de47
im-zyi <zhongyi_ooi990721@hotmail.com>
2019-12-12 16:39:46 +0800

    commit

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -91,4 +91,9 @@ class GameViewModel : ViewModel() {
     fun onGameFinish() {
         _eventGameFinish.value = true
     }
+    /** Method for the game completed event **/
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
 }
